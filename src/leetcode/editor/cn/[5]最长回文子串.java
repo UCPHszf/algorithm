e1350817ed02b34d3b25c31788515a1fce0894a7
UCPHszf <hsz921@alumni.ku.dk>
2022-09-13 13:35:11 +0200

    commit September 10th(leetcode 5)

---
+++ src/leetcode/editor/cn/[5]最长回文子串.java
@@ -0,0 +1,55 @@
+package leetcode.editor.cn;//给你一个字符串 s，找到 s 中最长的回文子串。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+//
+// Related Topics 字符串 动态规划 👍 5696 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int start = 0, end = 0, maxLen = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[i - 1][j + 1])) {
+                    dp[i][j] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
