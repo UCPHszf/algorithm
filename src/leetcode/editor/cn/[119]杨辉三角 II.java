13cebb0d27fd6c713831cfd04e0cc8d5ad14f7bd
UCPHszf <hsz921@alumni.ku.dk>
2022-09-13 12:34:50 +0200

    commit September 10th(leetcode 119)

---
+++ src/leetcode/editor/cn/[119]杨辉三角 II.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;//给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+//
+// 在「杨辉三角」中，每个数是它左上方和右上方的数的和。 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: rowIndex = 3
+//输出: [1,3,3,1]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: rowIndex = 0
+//输出: [1]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: rowIndex = 1
+//输出: [1,1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 0 <= rowIndex <= 33 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？ 
+//
+// Related Topics 数组 动态规划 👍 428 👎 0
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MathematicalTriangle119 {
+    public List<Integer> getRow(int rowIndex) {
+        ArrayList<Integer> row = new ArrayList<>();
+        for (int i = 0; i < rowIndex + 1; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) row.set(j, row.get(j) + row.get(j + 1));
+        }
+        return row;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
